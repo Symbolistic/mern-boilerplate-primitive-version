69088a0b34dfd1a04fa38467dd76e7c8ad555855
Symbol <Symbolistic@live.com>
2020-07-07 16:22:27 -0400

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://Symbol:test@cluster0-3t5ag.mongodb.net/BOILERPLATE?retryWrites=true&w=majority",
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require("./config/key")
+
+const { User } = require("./models/user");
+
+mongoose.connect(config.mongoURI,
                  { useNewUrlParser:true, useUnifiedTopology: true }).then(() => console.log("Connected to Database"))
                                                                     .catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
-});
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get('/', (req,res) => {
+    res.json({"hello ~": "Hi ~~lmkom"})
+})
+
+app.post("/api/users/register", (req,res) => {
+    const user = new User(req.body);
+
+    user.save((err, userData) => {
+        if (err) res.json ({ success: false, err })
+        res.status(200).json({
+                success:true
+            });
+    });
+
+    
+})
 
 app.listen(5000);
