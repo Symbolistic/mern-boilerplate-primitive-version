873e73b6b4754a9a41fb5471a203f998a99eea29
Symbol <Symbolistic@live.com>
2020-07-09 17:36:37 -0400

    resetting file and gonna start over trying to deploy to heroku

--- server/config/key.js
+++ server/config/key.js
@@ -1,8 +1,4 @@
-const express = require("express");
-const app = express();
-
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static('client/build'));
     module.exports = require("./prod");
 } else {
     module.exports = require("./dev");
