119fa3ddf0607dfb8dcacda3181e4c71c41af4ac
Symbol <Symbolistic@live.com>
2020-07-09 17:09:16 -0400

    still trying...

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use('*', express.static(path.join(__dirname, "client", "public")))
+app.use('*', express.static(path.join(__dirname, "client", "build")))
 
 
 app.get('/', (req, res) => {
